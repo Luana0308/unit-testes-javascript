5f13032269628771bfe870515380dd8d3986670d
Luana <luanaff-2008@hotmail.com>
2021-12-27 00:20:45 -0300

    desafio 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    //fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect (productDetails('param1','param2')).toBeInstanceOf(Array);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails()).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect( typeof productDetails('param1', 'param2')).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('param1', 'param2')[0].details.productId).toMatch(/123/);
   });
 });
